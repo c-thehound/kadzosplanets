21440713d04c528cab66c19fc0fbf38eaf58eeae
Castin <browncastin@gmail.com>
2019-04-12 09:03:53 +0300

    Add illustration field

--- stories/models.py
+++ stories/models.py
@@ -6,6 +6,7 @@ class Story(models.Model):
     title = models.CharField(max_length=500,blank=False)
     story_content = MartorField()
     uploaded = models.DateField(default=datetime.date.today)
+    illustration = models.ImageField(upload_to="illustrations")
     
     def __str__(self):
         return self.title

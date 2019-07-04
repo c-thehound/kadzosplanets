d80b95e0176fefc6f2f1fc245e77bb77bb1227c5
Castin <browncastin@gmail.com>
2019-07-04 13:05:35 +0300

    Fix urls

--- entry/urls.py
+++ entry/urls.py
@@ -5,5 +5,5 @@ from .views import *
 urlpatterns = [
     path('',landing,name="Landing page"),
     path('blog/',blog,name="Blog"),
-    re_path(r'^.well-known/acme-challenge/.*$',views.acme_challenge, name='acme-challenge'),
+    re_path(r'^.well-known/acme-challenge/.*$',acme_challenge, name='acme-challenge'),
 ]
